e02d63c5567adad74ef1ce7a65e2ac7e6866209f
wang_yanru <wang_yanru@venusgroup.com.cn>
2025-06-04 11:26:31 +0800

    chore: 初始化工程

---
+++ .vitepress/config.mjs
@@ -0,0 +1,109 @@
+import { defineConfig, loadEnv } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+
+export default defineConfig(({ command, mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    title: "wangyanru", // 站点标题(浏览器tab名称：title)
+    titleTemplate: "blog", // 站点标题描述(浏览器tab名称：title | titleTemplate)
+    description: "xxxxxxx", // 站点的描述 体现在HTML 中的 <meta> 标签。
+    // lang: 'en-US', // 语言 <html lang="en-US">
+    /**
+     * 开发模式下，使用绝对路径
+     * 生产环境下，站点将部署到的 base URL，以 / 开头和结尾，对应https://xiaotaokh.github.io/blog中的blog
+     */
+    base: mode === 'production' ? '/blog/' : '/',
+    // Vite 配置选项
+    vite: {
+      /**
+       * 静态资源文件夹，默认访问srcDir配置的路径, 构建是会放在outDir根目录，默认为public,
+       * srcDir配置为'.'或者'./'时，publicDir需要为public
+       * srcDir配置为'./src'时，publicDir需要为 ../public, 相对于src/去设置
+       */
+      publicDir: '../public',
+      // 插件
+      server: {
+        host: true, // 指定服务器应该监听哪个 IP 地址。 如果将此设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址。
+        port: env.VUE_APP_PORT || 5173, // 指定开发服务器端口。注意：如果端口已经被使用，Vite 会自动尝试下一个可用的端口，所以这可能不是开发服务器最终监听的实际端口。
+      },
+      // resolve: {
+      //   // alias 数组写法
+      //   alias: [
+      //     {
+      //       find: '@',
+      //       replacement: path => {
+      //         return join(__dirname, "src") // 配置路径别名，快捷指向相对路径
+      //       }
+      //     }
+      //   ],
+      //   // alias 对象写法
+      //   // alias: {
+      //   //   '@': join(__dirname, "src"), // 配置路径别名，快捷指向相对路径
+      //   // },
+      //   extensions: ['.js', '.vue', '.json', '.ts'] // 导入时想要省略的扩展名列表
+      // },
+    },
+
+    // @vitejs/plugin-vue 选项
+    vue: {},
+
+    // markdown解析器配置
+    markdown: {
+      container: {
+        tipLabel: '提示',
+        warningLabel: '警告',
+        dangerLabel: '危险',
+        infoLabel: '信息',
+        detailsLabel: '详细信息'
+      },
+      // lineNumbers: true, // 启用行号
+    },
+
+    head: [
+      ['link', { rel: 'stylesheet', href: '//at.alicdn.com/t/c/font_4743040_m7ehua33vvr.css' }], // iconfont
+      // 网站图标，相对于项目根目录，生产环境下需要base字段前缀
+      ['link', { rel: 'icon', href: `${ mode === 'production' ? '/blog/' : '/' }favicon/favicon.png` }],
+    ],
+    /**
+     * markdown文件所在的根目录，相对于项目根目录进行设置。不会直接影响 public 文件夹。
+     * . 和 ./ 一样，都代表根目录
+     * 配置为 ./src 后会导致public文件夹build后不出现在docs里（暂时不知道为啥），需要配置vite.publicDir='../public'
+     */
+    srcDir: "./src",
+    srcExclude: ['**/README.md', '**/TODO.md'], // 用于匹配应排除作为源内容输出的 markdown 文件
+    outDir: "./docs", // 项目的构建输出位置, 或者在package.json 中 scripts.build 配置 vitepress build --outDir docs
+    assetsDir: "static", // 指定build，在docs目录下放置生成的静态资源的文件夹名，仅影响在 Markdown 文件和组件中引入的静态资源（如图片、JS、CSS 等）
+    cacheDir: "./.vitepress/cache", // 缓存文件的目录，相对于项目根目录
+    metaChunk: true, // 当设置为 true 时，将页面元数据提取到单独的 JavaScript 块中，而不是内联在初始 HTML 中。
+    cleanUrls: true, // 当设置为 true 时，VitePress 将从 URL 中删除 .html 后缀
+    // 主题配置 https://vitepress.dev/reference/default-theme-config
+    themeConfig: {
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '示例', link: 'views/examples/markdown' }
+      ],
+
+      sidebar: [
+        {
+          text: '示例',
+          items: [
+            { text: 'Markdown 示例', link: 'views/examples/markdown' }
+          ]
+        }
+      ],
+
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/wyrzmz' }
+      ]
+    },
+
+    // 路由重定向
+    rewrites: {
+      'views/examples/:folder/(.*)': 'examples/:folder/(.*)' // :folder 是一个路径参数，表示文件夹名称。* 是一个通配符，它会匹配 views/examples/:folder/ 文件夹中的任何文件，无论文件名是什么。
+      // 可以写成如下形式，表示直接重定向views下所有的文件
+      // '^views/(.*)$': '(.*)', // 匹配 views 下所有文件并重定向
+    },
+  }
+})
---
+++ src/index.md
@@ -0,0 +1,96 @@
+---
+# https://xiaotaokh.github.io/blog
+
+# 页面的布局, home 主页的特殊布局  doc 默认文档样式 page 自定义布局
+layout: home
+
+# 当 layout 设置为 home 时，定义主页顶部的内容
+hero:
+  name: "xiaotaokh"
+  text: "vitePress"
+  tagline: 我的伟大项目标语
+  image:
+    src: /assets/home/home.jpeg
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /views/examples/markdown
+    - theme: alt
+      text: API 示例
+      link: /views/examples/api
+
+# 当 layout 设置为 home 时，定义主页底部特色内容
+features:
+  - icon: icon-gongneng-copy  # 图标
+    title: 特征 A  # 标题
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit  # 详情
+    link: /views/examples/markdown/a  # 外部内部链接
+    linkText: 更多 # 链接提示
+    
+  - title: 功能 B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 功能 C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
+<script setup>
+import { useData } from 'vitepress';
+
+const { theme, page, frontmatter } = useData()
+</script>
+
+[//]: # (<pre>{{ frontmatter }}</pre>)
+<i class="iconfont icon-gongneng-copy"></i>
+
+<style lang="scss">
+  :root {
+    --vp-home-hero-name-color: transparent;
+    --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe, #41d1ff);
+  }
+</style>
+
+1. [Link to baidu](https://www.baidu.com){target="_self"}
+2. [Link to baidu_外部](https://www.baidu.com){target="_bink"}
+
+
+
+
+1. [Home](/) <!-- 将用户导航至根目录下的 index.html -->
+2. [foo](views/examples/markdown/) <!-- 将用户导航至目录 foo 下的 index.html -->
+3. [foo heading](views/examples/markdown/#更多) <!-- 将用户锚定到目录 foo 下的index文件中的一个标题"更多"上 -->
+4. [bar - three](views/examples/markdown/a) <!-- 可以省略扩展名 -->
+5. [bar - three](views/examples/markdown/a.md) <!-- 可以添加 .md -->
+6. [bar - four](views/examples/markdown/a.html) <!-- 或者可以添加 .html -->
+
+| Tables        |      Are      |  Cool |
+| ------------- | :-----------: | ----: |
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      |   centered    |   $12 |
+| zebra stripes |   are neat    |    $1 |
+
+> 1. :tada:
+> 2. :100:
+
+
+[[toc]]
+
+::: info
+这是一个信息框。
+:::
+
+
+```js:line-numbers {1}
+// 启用行号
+const line2 = 'This is line 2'
+const line3 = 'This is line 3'
+```
+
+
+
+
+
+
+
+
+
